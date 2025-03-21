4ad147677293075998a95eeb79aba70180abff2d
Thipul Liyanage <thipuldamsilu@gmail.com>
2025-03-21 22:00:13 +0530

    finish splash screen setup

---
+++ app/src/main/java/com/booknest/community/activity/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.booknest.community.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.booknest.community.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+
+    }
+}
