bdd8721ed55f7f8ff209b50b7570d5f33657d142
Thipul Liyanage <thipuldamsilu@gmail.com>
2025-03-22 10:56:31 +0530

    SignUp UI Screen done

--- app/src/main/java/com/booknest/community/activity/SignUpActivity.kt
+++ app/src/main/java/com/booknest/community/activity/SignUpActivity.kt
@@ -1,6 +1,9 @@
 package com.booknest.community.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +19,15 @@ class SignUpActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val signInTextView: TextView = findViewById(R.id.textView20)
+        val createAccountBtn: Button = findViewById(R.id.button5)
+        signInTextView.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+        createAccountBtn.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
         }
     }
 }
